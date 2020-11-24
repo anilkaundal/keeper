17cab3284d6f82b2f621168e6d1bdc6b2f1f5ead
Anil Kaundal <60393858+anilkaundal@users.noreply.github.com>
2020-11-24 23:25:34 +0530

    Create components

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NoteAddRoundedIcon from "@material-ui/icons/NoteAddRounded";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <NoteAddRoundedIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
